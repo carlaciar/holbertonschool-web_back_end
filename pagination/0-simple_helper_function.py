faeecf0e000adcc463936a4e74ce2b3417a62326
carlaciar <carlaciarr@gmail.com>
2025-12-11 16:08:28 +1100

    fix pycodestyle

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """
-Function that helps with pagination. Returns the start and end indexes for a given page.
+Function that helps with pagination.
+Returns the start and end indexes for a given page.
 """
+
 
 def index_range(page: int, page_size: int):
     """
