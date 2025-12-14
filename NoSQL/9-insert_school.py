22c0a5db8e713cb1dc2d185d7789b9084a2ca5fd
carlaciar <carlaciarr@gmail.com>
2025-12-14 15:52:36 +1100

    add 9

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Inserts a new document in a MongoDB collection """
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a MongoDB collection
+    """
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
