cb88af38fce777bf11defc72566e37bd8c942f9f
carlaciar <carlaciarr@gmail.com>
2026-01-20 11:52:07 +1100

    Edit 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
 const database = process.argv[2];
 
@@ -14,13 +14,43 @@ const app = http.createServer((req, res) => {
   if (req.url === '/students') {
     res.write('This is the list of our students\n');
 
-    countStudents(database)
-      .then(() => {
-        res.end();
-      })
-      .catch((err) => {
-        res.end(`${err.message}\n`);
+    fs.readFile(database, 'utf-8', (err, data) => {
+      if (err) {
+        res.end('Cannot load the database\n');
+        return;
+      }
+
+      const lines = data
+        .split('\n')
+        .filter((line) => line.trim() !== '');
+
+      const students = lines.slice(1);
+
+      res.write(`Number of students: ${students.length}\n`);
+
+      const fields = {};
+
+      students.forEach((line) => {
+        const values = line.split(',');
+        const firstName = values[0];
+        const field = values[values.length - 1];
+
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstName);
       });
+
+      for (const field in fields) {
+        if (Object.prototype.hasOwnProperty.call(fields, field)) {
+          res.write(
+            `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`,
+          );
+        }
+      }
+
+      res.end();
+    });
 
     return;
   }
